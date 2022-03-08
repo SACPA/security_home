001b898d91176e812e1bf361d33da5c16429d0fd
Omar1019 <46832709+Omar1019@users.noreply.github.com>
2022-03-08 18:16:28 -0500

    mi avance de frontend

--- security_home/src/app/app-routing.module.ts
+++ security_home/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './paginas/login/login.component';
+import { RegistroComponent } from './paginas/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'login',
+    pathMatch:'full',
+  },
+  {
+    path:'login',
+    component:LoginComponent   
+
+  },
+  {
+    path:'registro',
+    component:RegistroComponent
+  },
+  
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
+
+  
 })
+
 export class AppRoutingModule { }
--- security_home/src/app/app.module.ts
+++ security_home/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './paginas/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +17,7 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+
