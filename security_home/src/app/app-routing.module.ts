03bdb7abf32c13cd85474d044af24d3162081329
Omar1019 <46832709+Omar1019@users.noreply.github.com>
2022-03-11 14:03:00 -0500

    bb

--- security_home/src/app/app-routing.module.ts
+++ security_home/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContraseniaolvidadaComponent } from './paginas/contraseniaolvidada/contraseniaolvidada.component';
-import { LoginComponent } from './paginas/login/login.component';
+import { HomePageComponent } from './paginas/home-page/home-page.component';
 import { PaneldeusuarioComponent } from './paginas/paneldeusuario/paneldeusuario.component';
 import { PerfilComponent } from './paginas/perfil/perfil.component';
 import { RegistroComponent } from './paginas/registro/registro.component';
@@ -10,12 +10,12 @@ import { RegistroComponent } from './paginas/registro/registro.component';
 const routes: Routes = [
   {
     path:'',
-    redirectTo:'login',
+    redirectTo:'home-page',
     pathMatch:'full',
   },
   {
-    path:'login',
-    component:LoginComponent   
+    path:'home-page',
+    component:HomePageComponent   
 
   },
   {
--- security_home/src/app/app.module.ts
+++ security_home/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomePageComponent } from './paginas/home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './paginas/registro/registro.component';
 import { PerfilComponent } from './paginas/perfil/perfil.component';
 import { ContraseniaolvidadaComponent } from './paginas/contraseniaolvidada/contraseniaolvidada.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @NgModule({
   declarations: [
+    HomePageComponent,
     AppComponent,
     RegistroComponent,
     PerfilComponent,
-    ContraseniaolvidadaComponent
+    ContraseniaolvidadaComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
